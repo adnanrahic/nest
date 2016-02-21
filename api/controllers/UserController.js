392bfbaecef58f76810bd15b1f221fdd0100e949
Adnan Rahić <ado.raha198@gmail.com>
2016-02-21 04:22:50 +0100

    form upload: done; show video page: done
    The form is finished, functionality wise. Styling is needed. The show video page is functional, used iframe for youtube embedding. Still have to finish -10sec, +10sec buttons, and add styling.

---
+++ api/controllers/UserController.js
@@ -0,0 +1,19 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	'new': function (req, res) {
+        res.view();
+    },
+    create: function (req, res, next) {
+        User.create(req.params.all(), function userCreated(err, video) {
+            if (err) return next(err);
+            res.json(video);
+        });
+    }
+};
+
